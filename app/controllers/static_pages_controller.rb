36886892cfc0d40bc3660d255ade0bb4cff4fd8b
paterneluciano1 <123613532+paterneluciano1@users.noreply.github.com>
2025-03-14 18:36:18 +0100

    add files

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,30 @@
+class StaticPagesController < ApplicationController
+    
+    require 'fedapay'
+  
+    def create
+
+      # Vous pouvez envisager de déplacer ces configurations dans un initializer
+      FedaPay.api_key = 'api_secret_key'
+
+      FedaPay.environment = 'environment' # Ou 'live' en production
+  
+      
+        transaction = FedaPay::Transaction.create(
+            amount: 100000,
+            currency: { iso: 'XOF' },
+            callback: "http://localhost:3000/",
+            description: 'Achat de montre de luxe',
+        )
+        
+
+        # Génération du token de la transaction
+        token = transaction.generate_token
+
+        # Redirection automatique vers la page de paiement
+        redirect_to token.url, allow_other_host: true
+
+        puts "Transaction successfully created : #{transaction.inspect}"  
+    end
+  end
+  
